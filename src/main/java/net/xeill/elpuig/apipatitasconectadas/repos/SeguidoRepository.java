243eddd0c6c4cbb5edfbb6016c5710bc741f2a69
Fernandodg97 <ferdiaz1997@gmail.com>
2025-04-08 20:57:57 +0200

    feat: Se comienza Seguido

---
+++ src/main/java/net/xeill/elpuig/apipatitasconectadas/repos/SeguidoRepository.java
@@ -0,0 +1,12 @@
+package net.xeill.elpuig.apipatitasconectadas.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.xeill.elpuig.apipatitasconectadas.models.*;
+
+//Clase que me permite realizar las consultas a la base de datos
+@Repository
+public interface SeguidoRepository extends JpaRepository<SeguidoModel, Long> {
+
+} 
