c07a2f81727a23dd90057def29ff4638a51b943c
Fernandodg97 <ferdiaz1997@gmail.com>
2025-04-11 19:05:25 +0200

    feat: Se mejoran rutas Perfil

--- src/main/java/net/xeill/elpuig/apipatitasconectadas/controllers/PerfilController.java
+++ src/main/java/net/xeill/elpuig/apipatitasconectadas/controllers/PerfilController.java
@@ -11,28 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.xeill.elpuig.apipatitasconectadas.models.*;
 import net.xeill.elpuig.apipatitasconectadas.services.*;
 
 @RestController
-@RequestMapping("/perfiles")
 public class PerfilController {
 
     @Autowired
     private PerfilService perfilService;
 
     // Método GET para obtener todos los perfiles
-    @GetMapping
+    @GetMapping(path = "/perfiles")
     public ArrayList<PerfilModel> getPerfiles() {
         // Llama al servicio para obtener la lista de perfiles y la retorna
         return this.perfilService.getPerfiles();
     }
 
     // Método POST para guardar un nuevo perfil
-    @PostMapping
+    @PostMapping(path = "/perfiles")
     public ResponseEntity<?> savePerfil(@RequestBody PerfilModel perfil) {
         try {
             // Intenta guardar el perfil recibido en el cuerpo de la petición
@@ -47,21 +45,21 @@ public class PerfilController {
     }
 
     // Método GET para obtener un perfil por su ID
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/usuarios/{id}/perfiles")
     public Optional<PerfilModel> getPerfilById(@PathVariable("id") Long id) {
         // Llama al servicio para buscar el perfil por ID
         return this.perfilService.getById(id);
     }
 
     // Método POST para actualizar un perfil existente según su ID
-    @PostMapping(path = "/{id}")
+    @PostMapping(path = "/usuarios/{id}/perfiles")
     public PerfilModel updatePerfilById(@RequestBody PerfilModel request, @PathVariable("id") Long id) {
         // Llama al servicio para actualizar el perfil con los nuevos datos
         return this.perfilService.updateByID(request, id);
     }
 
     // Método DELETE para eliminar un perfil por su ID
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/usuarios/{id}/perfiles")
     public String deleteById(@PathVariable("id") Long id) {
         // Intenta eliminar el perfil con el ID especificado
         boolean ok = this.perfilService.deletePerfil(id);
